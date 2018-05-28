cef8eb7d60ad2a8088d9601f89195b2914877f87
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-28 13:20:30 -0400

    Solved lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
-  end
+def sentence?
+  self.end_with?(".")
+end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    var = self.split(%r{\?+\s|\!+\s|\.\s}).uniq
+    var.count
+    # binding.pry
+    # true
   end
 end
